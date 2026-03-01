923e43c63b8c767732f1ecc07bfcc3fbe586b1de
Ahmed Panwala <87858980+AhmedPanwala@users.noreply.github.com>
2026-03-01 11:43:16 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://course-management-system-2-8hmu.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
